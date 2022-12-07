26c66f9e24aafc28eb993400fe05200bbecb7de5
gasca14 <alexgasca89@gmail.com>
2022-12-06 18:39:07 -0600

    crud completo

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,47 +1,58 @@
-const {response, request} = require("express");
+const { response, request } = require("express");
 const Usuario = require('../models/Usuario.model')
 
-const usuariosGet = (req = request, res = response)=>{
-    res.status(200).json({
-        msg: 'GET API - controlador'
-    })
+const usuariosGet = async (req = request, res = response) => {
+
+    try {
+        const usuarios = await Usuario.find()
+        res.status(200).json({
+            msg: 'Usuarios obtenidos de la base de datos',
+            detalle: usuarios
+        })
+    } catch (error) {
+        res.status(400).json({
+            msg: 'se detecto un error',
+            detalle: error.message
+        })
+    }
 }
 
-const usuariosPost = async(req = request,res = response)=>{
-   try {
-    const body = req.body
-    const usuario = new Usuario(body)
-    await usuario.save()
-    res.status(200).json({
-        msg: 'POST API - controlador',
-        post:body,
-        usuario: usuario
-    });
-   } catch (error) {
-    res.status(400).json({
-        msg: "se detecto un error",
-        detalle: error.message
-    })
-   }
+const usuariosPost = async (req = request, res = response) => {
+    try {
+        const body = req.body
+        const usuario = new Usuario(body)
+        await usuario.save()
+        res.status(200).json({
+            msg: 'Usuario agregado',
+            post: body,
+            usuario: usuario
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg: "se detecto un error",
+            detalle: error.message
+        })
+    }
 
 }
 
-const usuariosPut = (req = request,res = response)=>{
+const usuariosPut = async (req = request, res = response) => {
     const { id } = req.params;
     const body = req.body;
-    console.log(req.body);
+    const usuario = await Usuario.findByIdAndUpdate(id, body)
+    // console.log(req.body);
     res.status(200).json({
-        msg: 'PUT API - controlador',
-        id: id,
-        body: body
+        msg: 'Usuario actualizado',
+        usuario
     });
 }
 
-const usuariosDelete = (req = request,res = response)=>{
+const usuariosDelete = async (req = request, res = response) => {
     const { id } = req.params;
+    const usuario =  await Usuario.findByIdAndDelete(id)
     res.status(200).json({
-        msg: 'DELETE API - controlador',
-        id
+        msg: 'El usuario fue eliminado',
+        usuario
     });
 }
 
