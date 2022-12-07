fce581583030d2eabd17d9e94721005c2af75207
gasca14 <alexgasca89@gmail.com>
2022-12-07 17:50:05 -0600

    haciendo validacion de contrasena para verificar si es la correcta o no

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,15 @@
 const { request, response} = require('express');
 const Usuario = require('../models/Usuario.model');
+const authModel = require('../models/Auth.model')
 
 const login = async (req = request, res = response) => {
     const {correo,password} = req.body;
     const userInformation = await Usuario.findOne({email: correo});
+    const validPassword = await authModel.comparePassword(password, userInformation.password)
+
     res.status(200).json({
         msg:"Entro login",
-        informacion_usuario: userInformation
+        is_valid:validPassword
     })
 }
 
--- models/Auth.model.js
+++ models/Auth.model.js
@@ -5,6 +5,12 @@ const hashPassword = async (password) => {
     return await bcrypt.hash(password, saltRounds)
 };
 
+const comparePassword = async (password, hash) => {
+
+    return await bcrypt.compare(password, hash)
+}
+
 module.exports = {
-    hashPassword
+    hashPassword,
+    comparePassword
 }
