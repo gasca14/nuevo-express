8f672a5608aa2e0179c66928fc170e5ef269d128
gasca14 <alexgasca89@gmail.com>
2022-12-07 22:58:29 -0600

    agregando validacion if/else

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,25 @@
-const { request, response} = require('express');
+const { request, response } = require('express');
 const Usuario = require('../models/Usuario.model');
 const authModel = require('../models/Auth.model')
 
 const login = async (req = request, res = response) => {
-    const {correo,password} = req.body;
-    const userInformation = await Usuario.findOne({email: correo});
+    const { correo, password } = req.body;
+    const userInformation = await Usuario.findOne({ email: correo });
     const validPassword = await authModel.comparePassword(password, userInformation.password)
 
-    res.status(200).json({
-        msg:"Entro login",
-        is_valid:validPassword
-    })
+    if (validPassword === true) {
+        res.status(200).json({
+            msg: "Entro login",
+            is_valid: validPassword,
+            data: "token"
+        })
+    } else {
+        res.status(401).json({
+            msg:"Error, contraseña incorrecta",
+            is_valid:validPassword
+        })
+    }
+
 }
 
 module.exports = {
